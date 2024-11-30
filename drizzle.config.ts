b106ade6486b79ea1f3e58eec04b8200c72d312d
Heath Babb <heath@heath-babb.tech>
2024-11-30 12:02:15 -0500

    feat: added t3-env to the project
    Added t3-env server environmental variables type safety with zod
    converted all process.env to .env using the callback from src/env/server.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'drizzle-kit'
+import { env } from "@/env/server";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
-  out: './src/db/drizzle',
-  dialect: 'postgresql',
+  schema: "./src/db/schema.ts",
+  out: "./src/db/drizzle",
+  dialect: "postgresql",
   dbCredentials: {
     // biome-ignore lint/style/noNonNullAssertion: <explanation>
-    url: process.env.DATABASE_URL!,
+    url: env.DATABASE_URL!,
   },
-})
+});
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,15 @@
-import { neon } from '@neondatabase/serverless'
-import { config } from 'dotenv'
-import { drizzle } from 'drizzle-orm/neon-http'
+import { env } from "@/env/server";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
 
-config({ path: '.env.local' })
+config({ path: ".env.local" });
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
-const sql = neon(process.env.DATABASE_URL!)
+const sql = neon(env.DATABASE_URL!);
 
 // logger
 // const db = drizzle(sql, { logger: true })
-const db = drizzle(sql)
+const db = drizzle(sql);
 
-export { db }
+export { db };
